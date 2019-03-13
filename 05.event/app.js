6de3a09f96d027341754173924ba290b38935fb0
song <qhftkf1@naver.com>
2019-03-13 17:27:15 +0900

    Second commit

---
+++ 05.event/app.js
@@ -0,0 +1,21 @@
+new Vue({
+  el:'#vue-app',
+  data:{
+    name:'song',
+    age:28,
+    x:0,
+    y:0
+  },
+  methods:{
+    Increase: function(inc){
+      return this.age += inc;
+    },
+    Decrease: function(dec){
+      return this.age -= dec;
+    },
+    updateXY:function(event){
+     this.x = event.offsetX;
+     this.y = event.offsetY;
+    }
+  }
+  });
