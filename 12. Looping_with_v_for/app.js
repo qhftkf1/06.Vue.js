1f00a82d49d217682833ac09d0d666f1c3db9296
song <qhftkf1@naver.com>
2019-03-18 18:31:45 +0900

    Third commit

---
+++ 12. Looping_with_v_for/app.js
@@ -0,0 +1,14 @@
+new Vue({
+  el:'#vue-app',
+  data:{
+    characters:['song','seung','gi','kang'],
+    ninjas:[
+      {name:'s',age:28},
+      {name:'young',age:18},
+      {name:'yu',age:25}
+    ]
+  },
+  computed:{
+
+  }
+  });
